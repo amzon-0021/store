8dc4605167f119be0df5153c920b9c754bb3940d
amzon-0021 <58093149+amzon-0021@users.noreply.github.com>
2021-01-23 18:36:51 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
-print("complete")
-input()
+print("DOS-Betaを初期化します")
+import os
+open("store.ini","w")
+s = open("config.ini","w")
+s.write("""[Defalt]
+application = True
+Unsignedapplication = false
+nostoreapp = 
+appcmd = None""")
+
+
+
