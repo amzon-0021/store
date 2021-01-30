3c790cb4d673178f50f25320940cc9390cb74813
amzon-0021 <58093149+amzon-0021@users.noreply.github.com>
2021-01-30 14:54:21 +0900

    Add files via upload

---
+++ update.py
@@ -0,0 +1,24 @@
+print("auif UPloder Ver.10")
+print("AUIFファイルを使用してアップデートをします")
+import configparser
+import codecs
+import urllib.request
+def install(app):
+    try:
+        open("test.py","w")
+        d = config["Defalt"][app]
+        urllib.request.urlretrieve(d, "DOS.py")
+    except KeyError as e:
+        print(f"{app}は存在しません")
+print("使用可能なAUIFを検索しています")
+#urllib.request.urlretrieve("https://github.com/amzon-0021/store/raw/main/update.ini", "update.ini")
+config = configparser.ConfigParser()
+###config.read('update.ini')
+config.readfp(codecs.open("update.ini", "r", "utf8"))
+print("ダウンロードするバージョンを選択してください(Ver.10.2以上のみ使用できます)")
+for key in config['Defalt']:
+    print(key)
+c = input(">>")
+install(c)
+
+
